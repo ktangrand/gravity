8a364cd5521906920e113f808b8a335aaaf45fed
ktangrand <65399907+ktangrand@users.noreply.github.com>
2025-06-06 15:51:02 +0200

    Center camera based on world size

--- game-map.js
+++ game-map.js
@@ -1,6 +1,7 @@
 // =================================================================
 // Generate new world
 // =================================================================
+const WORLD_SIZE = 1;
 
 function getRandomInt (min, max) {
   return Math.floor(Math.random() * (max - min) + min);
@@ -46,8 +47,8 @@ function createWorld () {
   // Random planets
   for (let nr = 1; nr < 100; nr++) {
     planets.push({
-      x: Math.random(),
-      y: Math.random(),
+      x: Math.random() * WORLD_SIZE,
+      y: Math.random() * WORLD_SIZE,
       populated: null,
       nr,
       ...generateResources()
@@ -58,6 +59,7 @@ function createWorld () {
   const world = {
     fieldResolution,
     G_CONSTANT: 0.00000004,
+    size: WORLD_SIZE,
     planets
   };
 
@@ -67,7 +69,7 @@ function createWorld () {
   const buffer = new ArrayBuffer(fieldResolution ** 2 * 4 * 2);
   const fxF32 = new DataView(buffer);
   const fyF32 = new DataView(buffer, buffer.byteLength / 2);
-  const worldStep = 1 / (fieldResolution - 1);
+  const worldStep = WORLD_SIZE / (fieldResolution - 1);
   for (let y = 0; y < fieldResolution; y++) {
     const rowOfs = y * fieldResolution;
     for (let x = 0; x < fieldResolution; x++) {
